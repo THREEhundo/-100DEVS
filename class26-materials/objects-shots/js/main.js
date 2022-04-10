545525136dc8090ada8efb8bb33f7e491a5bcd7a
THREEhundo <samuelbaik87@gmail.com>
2022-04-10 00:46:57 -0700

    added fetched info to html

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,15 +1,23 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+let cocktailName = document.querySelector('h2')
+let cocktailImg = document.querySelector('img')
+let coctailInstructionsHeader = document.querySelector('h3')
+let cocktailInstructions = document.createElement('p')
 
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-	.then((res) => res.json())
-	.then((data) => console.log(data.drinks[0]))
+coctailInstructionsHeader.appendChild(cocktailInstructions)
 
 // ! GET COCKTAILS
 const getCocktails = () => {
 	const userInput = document.querySelector('input').value
 	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`)
 		.then((res) => res.json())
-		.then((data) => console.log(data.drinks[0]))
+		.then((data) => {
+			console.log(data.drinks[0])
+			cocktailName.innerHTML = data.drinks[0].strDrink
+			cocktailImg.src = data.drinks[0].strDrinkThumb
+			cocktailImg.alt = data.drinks[0].strDrink
+			cocktailInstructions.innerHTML = data.drinks[0].strInstructions
+		})
 }
 
 const btn = document
