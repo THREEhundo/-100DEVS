2d6442d0308a8bc6634465c5b2fff9dc5b32ae99
THREEhundo <samuelbaik87@gmail.com>
2022-04-10 00:34:00 -0700

    get cocktails from user input

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,6 +1,24 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+	.then((res) => res.json())
+	.then((data) => console.log(data.drinks[0]))
+
 // ! GET COCKTAILS
+const getCocktails = () => {
+	const userInput = document.querySelector('input').value
+	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`)
+		.then((res) => res.json())
+		.then((data) => console.log(data.drinks[0]))
+}
+
+const btn = document
+	.querySelector('button')
+	.addEventListener('click', () => getCocktails())
+
+const inputEnter = document
+	.querySelector('input')
+	.addEventListener('keypress', (e) => e.key === 'Enter' && getCocktails())
 
 // ! MAKE A CAROUSEL
 
