93b7ba24af610f8aaebbf79d6dad55136b8c6764
THREEhundo <samuelbaik87@gmail.com>
2022-04-10 17:07:12 -0700

    random cocktail variation bug fixed

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -2,7 +2,13 @@
 
 fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
 	.then((res) => res.json())
-	.then((data) => console.log(data.drinks[0]))
+	.then((data) => {
+		let totalDrinks = data.drinks.length
+		let random = Math.random()
+		let randomIndex = Math.floor(random * totalDrinks)
+		let randomDrink = data.drinks[randomIndex]
+		console.log(randomDrink)
+	})
 
 let cocktailName = document.querySelector('h2')
 let cocktailImg = document.querySelector('img')
@@ -13,41 +19,51 @@ let video = document.querySelector('embed')
 
 coctailInstructionsHeader.appendChild(cocktailInstructions)
 
+document
+	.querySelector('button[name="random-drink"]')
+	.addEventListener('click', () => getCocktails())
+
 // ! GET COCKTAILS
 const getCocktails = () => {
 	const userInput = document.querySelector('input').value
 	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`)
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data.drinks[0])
-			cocktailName.innerHTML = data.drinks[0].strDrink
-			cocktailImg.src = data.drinks[0].strDrinkThumb
-			cocktailImg.alt = data.drinks[0].strDrink
-			cocktailInstructions.innerHTML = data.drinks[0].strInstructions
-			for (const property in data.drinks[0]) {
+			let drinksLength = data.drinks.length
+			console.log(drinksLength)
+			let fetchedDrink =
+				randomDrink(drinksLength, data.drinks) || data.drinks[0]
+			cocktailName.innerHTML = fetchedDrink.strDrink
+			cocktailImg.src = fetchedDrink.strDrinkThumb
+			cocktailImg.alt = fetchedDrink.strDrink
+			cocktailInstructions.innerHTML = fetchedDrink.strInstructions
+			ingredients.innerHTML = ''
+			ingredients.innerHTML = 'Ingredients'
+
+			for (const property in fetchedDrink) {
 				if (
 					property.includes('strIngredient') &&
-					data.drinks[0][property] !== null
+					fetchedDrink[property] !== null
 				) {
 					let ingredient = document.createElement('li')
-					ingredient.innerHTML = data.drinks[0][property]
+					ingredient.innerHTML = fetchedDrink[property]
 					ingredients.appendChild(ingredient)
 				}
-				if (property === 'strVideo' && data.drinks[0]['strVideo'] !== null) {
-					console.log(data.drinks[0]['strVideo'])
-					const embedLink = data.drinks[0]['strVideo'].replace(
+				if (property === 'strVideo' && fetchedDrink['strVideo'] !== null) {
+					console.log(fetchedDrink['strVideo'])
+					const embedLink = fetchedDrink['strVideo'].replace(
 						'watch?v=',
 						'embed/',
 					)
 					video.src = embedLink
 				}
 			}
-			//data.drinks[0].
 		})
+		.catch((err) => console.error(err))
 }
 
 const btn = document
-	.querySelector('button')
+	.querySelector('button[name="button"]')
 	.addEventListener('click', () => getCocktails())
 
 const inputEnter = document
@@ -56,6 +72,51 @@ const inputEnter = document
 
 // ! MAKE A CAROUSEL
 
+function randomDrink(drinks) {
+	console.log(drinks)
+	let totalDrinks = drinks.length
+	let random = Math.random()
+	let randomIndex = Math.floor(random * totalDrinks)
+	let randomDrink = drinks[randomIndex]
+	return randomDrink
+}
+const randomDrinkMaker = () => {
+	const userInput = document.querySelector('input').value
+	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`)
+		.then((res) => res.json())
+		.then((data) => {
+			let drinks = data.drinks
+			let totalDrinks = drinks.length
+			let rand = Math.random()
+			let randomIndex = Math.floor(rand * totalDrinks)
+			let randomDrink = drinks[randomIndex]
+			cocktailName.innerHTML = randomDrink.strDrink
+			cocktailImg.src = randomDrink.strDrinkThumb
+			cocktailImg.alt = randomDrink.strDrink
+			cocktailInstructions.innerHTML = randomDrink.strInstructions
+			ingredients.innerHTML = ''
+			ingredients.innerHTML = 'Ingredients'
+			for (const property in randomDrink) {
+				if (
+					property.includes('strIngredient') &&
+					randomDrink[property] !== null
+				) {
+					let ingredient = document.createElement('li')
+					ingredient.innerHTML = randomDrink[property]
+					ingredients.appendChild(ingredient)
+				}
+				if (property === 'strVideo' && randomDrink['strVideo'] !== null) {
+					console.log(randomDrink['strVideo'])
+					const embedLink = randomDrink['strVideo'].replace(
+						'watch?v=',
+						'embed/',
+					)
+					video.src = embedLink
+				}
+			}
+		})
+		.catch((err) => console.log(`error ${err}`))
+}
 // ! PLAY AROUND WITH THE DATABASE
 
 // ! BUILD 3 APPS
