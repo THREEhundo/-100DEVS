bd03c2b875bf7700a750fcb8141c8ec8d42a2447
THREEhundo <samuelbaik87@gmail.com>
2022-04-12 19:48:41 -0700

    shows both player and dealer totals

--- class27-materials/deck-of-cards-api/js/main.js
+++ class27-materials/deck-of-cards-api/js/main.js
@@ -23,6 +23,11 @@ shuffleCards.addEventListener('click', () => getFetch())
 
 const dealCards = document.querySelector('button[name="deal"]')
 dealCards.addEventListener('click', () => dealFourCards())
+//
+//let card1 = document.querySelector('#card1')
+//let card2 = document.querySelector('#card2')
+//let card3 = document.querySelector('#card3')
+//let card4 = document.querySelector('#card4')
 
 const dealFourCards = () => {
 	let draw = fetch(
@@ -36,6 +41,14 @@ const dealFourCards = () => {
 			let card2 = (document.querySelector('#card2').src = data.cards[1].image)
 			let card3 = (document.querySelector('#card3').src = data.cards[2].image)
 			let card4 = (document.querySelector('#card4').src = data.cards[3].image)
+			let card1Val = cardScore(data.cards[0].value)
+			let card2Val = cardScore(data.cards[1].value)
+			let card3Val = cardScore(data.cards[2].value)
+			let card4Val = cardScore(data.cards[3].value)
+			let dealerTotal = totalScore(card1Val, card2Val)
+			let playerTotal = totalScore(card3Val, card4Val)
+			document.querySelector('#dealer-total').innerHTML = dealerTotal
+			document.querySelector('#player-total').innerHTML = playerTotal
 			document.querySelector('#cards-left').innerHTML = data.remaining
 		})
 	console.log(draw.cards)
@@ -44,3 +57,15 @@ const dealFourCards = () => {
 // ! HW
 // Store deck into local storage
 // 'deck_id'
+function cardScore(card) {
+	return card === 'ACE' ||
+		card === 'KING' ||
+		card === 'QUEEN' ||
+		card === 'JACK'
+		? 10
+		: Number(card)
+}
+
+function totalScore(card1, card2) {
+	return card1 + card2
+}
