57d9218ee6ecba30e19f389641745cbbad29ede3
THREEhundo <samuelbaik87@gmail.com>
2022-04-14 20:22:26 -0700

    when player stays, algo makes computer hit until it hits 17 or busts

--- class27-materials/deck-of-cards-api/js/main.js
+++ class27-materials/deck-of-cards-api/js/main.js
@@ -50,6 +50,8 @@ let result = document.querySelector('#result')
 let hiddenCard = document.querySelector('#hidden-card')
 let dealerTotal = document.querySelector('#dealer-total')
 let dealerPartialTotal = document.querySelector('#dealer-partial-total')
+let dealerCards = document.querySelector('.dealer-cards')
+let playerCards = document.querySelector('.player-cards')
 //let card1Val, card2Val, card3Val, card4Val
 
 const dealFourCards = () => {
@@ -115,18 +117,56 @@ function flipDealersCard() {
 	card1.classList.remove('none')
 	dealerPartialTotal.classList.add('none')
 	dealerTotal.classList.toggle('none')
-	if (dealer.total() === 21)
+	if (dealer.total() === 21 && player.total() < 21)
 		result.innerHTML = 'Dealer has Blackjack! You Lose!'
-	if (dealer.total() > 21) result.innerHTML = 'Dealer busted! You Win!'
-	if (dealer.total() < 17) return /* fetch again return */
+	if (dealer.total() > 21 && player.total() <= 21)
+		result.innerHTML = 'Dealer busted! You Win!'
+	if (dealer.total() < 17 && player.total() <= 21)
+		dealerFirstHit() /* fetch again return */
 }
 
-function dealerHits() {
+// ! Use data to count card and keep adding?
+function dealerFirstHit() {
 	fetch(
 		`https://deckofcardsapi.com/api/deck/${localStorage.getItem(
 			'deck_id',
 		)}/draw/?count=1`,
 	)
+		.then((res) => res.json())
+		.then((data) => {
+			let nextFetchedCard = data.cards[0]
+			nextFetchedCard.numVal = cardScore(data.cards[0].value)
+			dealer.hand = [...dealer.hand, nextFetchedCard]
+			dealerTotal.innerHTML = dealer.total()
+
+			let nextCard = document.createElement('img')
+			nextCard.src = nextFetchedCard.image
+
+			dealerCards.appendChild(nextCard)
+			//if (
+			//	(dealer.total() === 17 && dealer.total() > player.total()) ||
+			//	(dealer.total() > 17 && dealer.total() < 22 && dealer.total()) >
+			//		player.total()
+			//)
+			//	result.innerHTML = 'Dealer Wins!'
+			if (
+				dealer.total() >= 17 &&
+				dealer.total() <= 21 &&
+				dealer.total() > player.total()
+			)
+				result.innerHTML = 'Dealer Wins!'
+
+			if (dealer.total() > 21) result.innerHTML = 'You Win! Dealer Busts'
+			if (dealer.total() < 17 && player.total() <= 21) dealerFirstHit()
+			if (
+				dealer.total() >= 17 &&
+				dealer.total <= 21 &&
+				player.total() < dealer.total
+			)
+				result.innerHTML = `Dealer Wins`
+			if (dealer.total >= 21 && player.total() > dealer.total())
+				result.innerHTML = `Player Wins`
+		})
 }
 // show winner
 // hit -> fetch another card and add to player 1
