03dc0c1f3a1e225541fc23a2f694154e7bf2aaaf
THREEhundo <samuelbaik87@gmail.com>
2022-04-30 15:59:12 -0700

    Daily Kata

--- Additional files/Codewars Kata's/codewars.js
+++ Additional files/Codewars Kata's/codewars.js
@@ -240,3 +240,18 @@ const findNeedle = (haystack) => {
 
 const findNeedleBestPractice = (haystack) =>
 	`found the needle at position ${haystack.indexOf('needle')}`
+
+/****************************/
+/*** Ones and Zeros		5/3
+/***************************/
+
+/***
+ * Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+ *
+ * Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+ *
+ * However, the arrays can have varying lengths.
+ */
+
+// !BEST PRACTICES
+const binaryArrayToNumber = (arr) => parseInt(arr.join(''), 2)
