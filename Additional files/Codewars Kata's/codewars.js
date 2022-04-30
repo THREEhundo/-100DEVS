d699f9d7997c09d19896d6b14c50f35784b77fd9
THREEhundo <samuelbaik87@gmail.com>
2022-04-29 22:03:59 -0700

    filter kata

--- Additional files/Codewars Kata's/codewars.js
+++ Additional files/Codewars Kata's/codewars.js
@@ -138,3 +138,30 @@ const bestPracticeSolution = (number) => {
 	}
 	return sum
 }
+
+/****************************/
+/*** List Filtering			4/29
+/***************************/
+
+/***
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+ */
+
+function filter_list(l) {
+	return l.filter((item) => {
+		console.log(typeof 0)
+		if (typeof item === 'number' || item === 0) {
+			return item
+		} else {
+			return ''
+		}
+	})
+}
+let test = filter_list([1, 'a', 'b', 0, 15])
+console.log(test)
+// Why does 0 not pass the test?
+
+// !BEST PRACTICE
+
+const filter_list_best_practice = (l) =>
+	l.filter((item) => typeof item == 'number')
