5dc21f49200af4bfec0cc8139f18d25f0843947f
THREEhundo <samuelbaik87@gmail.com>
2022-05-01 14:24:27 -0700

    daily kata

--- Additional files/Codewars Kata's/codewars.js
+++ Additional files/Codewars Kata's/codewars.js
@@ -165,3 +165,19 @@ console.log(test)
 
 const filter_list_best_practice = (l) =>
 	l.filter((item) => typeof item == 'number')
+
+/****************************/
+/*** Grasshopper - Summation			4/29
+/***************************/
+
+/***
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+ */
+var summation = function (num) {
+	let result = 0
+	for (var i = 1; i <= num; i++) {
+		result += i
+	}
+
+	return result
+}
