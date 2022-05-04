067536072bd11e765f51e0ac2e9c790f9f416825
THREEhundo <samuelbaik87@gmail.com>
2022-05-03 17:11:46 -0700

    practicing extends

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,30 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 
+class Animal {
+	constructor(name) {
+		this.name = name
+	}
+	speak() {
+		console.log(`${this.name} makes a sound`)
+	}
+}
+
+class Cat extends Animal {
+	constructor(genius) {
+		super(Animal)
+		this.genius = genius
+	}
+	types() {
+		console.log(`there are thousands of types of cats`)
+	}
+}
+
+class Tiger extends Cat {
+	constructor(colors) {
+		super(Cat)
+		this.colors = colors
+	}
+	stalk() {
+		console.log(`Slowly walks among the fern`)
+	}
+}
