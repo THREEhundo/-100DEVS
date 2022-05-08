9ca18a8042d77a3673a7be91005526cdc2479b7b
THREEhundo <samuelbaik87@gmail.com>
2022-05-08 04:51:02 -0700

    backdated commit

--- Additional files/Codewars Kata's/codewars.js
+++ Additional files/Codewars Kata's/codewars.js
@@ -263,6 +263,7 @@ const binaryArrayToNumber = (arr) => parseInt(arr.join(''), 2)
 Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
  */
 
+// Wrote best practice solution!
 // changed function argument to this so that split does not return undefined
 String.prototype.toJadenCase = function () {
 	return this.split(' ')
