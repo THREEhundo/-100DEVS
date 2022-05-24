2318808c01789d069c7acc143d32933ad8bf48a5
THREEhundo <samuelbaik87@gmail.com>
2022-05-24 15:07:41 -0700

    refactor demo

--- class35-materials/node-intro-demo/demo.js
+++ class35-materials/node-intro-demo/demo.js
@@ -1,9 +1,10 @@
-const http = require('http')
-const fs = require('fs')
-http.createServer((req, res) => {
-  fs.readFile('demofile.html', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(data)
-    res.end()
-  })
+import { createServer } from 'http'
+import { readFile } from 'fs'
+
+createServer((req, res) => {
+	readFile('demofile.html', (err, data) => {
+		res.writeHead(200, { 'Content-Type': 'text/html' })
+		res.write(data)
+		res.end()
+	})
 }).listen(8000)
