0eeee7dd60b409d757d514490345920ec2aace20
THREEhundo <samuelbaik87@gmail.com>
2022-05-24 15:11:08 -0700

    hw

--- class35-materials/node-backend-simple-json/js/main.js
+++ class35-materials/node-backend-simple-json/js/main.js
@@ -1,13 +1,17 @@
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+const clickMe = document.querySelector('#clickMe')
+const personName = document.querySelector('#personName').textContent
+const personStatus = document.querySelector('#personStatus').textContent
+const personOccupation = document.querySelector('#personOccupation').textContent
 
-async function makeReq(){
+clickMe.addEventListener('click', makeReq)
 
-  const userName = document.querySelector("#userName").value;
-  const res = await fetch(`/api?student=${userName}`)
-  const data = await res.json()
+async function makeReq() {
+	const userName = document.querySelector('#userName').value
+	const res = await fetch(`/api?student=${userName}`)
+	const data = await res.json()
 
-  console.log(data);
-  document.querySelector("#personName").textContent = data.name
-  document.querySelector("#personStatus").textContent = data.status
-  document.querySelector("#personOccupation").textContent = data.currentOccupation
+	console.log(data)
+	personName = data.name
+	personStatus = data.status
+	personOccupation = data.currentOccupation
 }
